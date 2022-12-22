f8f8d088d289f7be7be5a989a08bb8b1790c5fad
Keith T. Garner <kgarner@kgarner.com>
2022-12-22 17:04:29 -0600

    fix helper spec for openhab_automation_search_paths

--- spec/openhab/rspec/helpers_spec.rb
+++ spec/openhab/rspec/helpers_spec.rb
@@ -4,8 +4,10 @@
 RSpec.describe OpenHAB::RSpec::Helpers do
   describe "load_rules" do
     it "respects start levels" do
+      allow(::RSpec.configuration).to receive(:openhab_automation_search_paths).and_return(["automation"])
       allow(Dir).to receive(:[]).and_return(%w[automation/filea.rb automation/sl30/filec.rb
                                                automation/filea.sl20.rb automation/sl30/fileb.rb])
+
       # rubocop:disable RSpec/SubjectStub
       expect(subject).to receive(:load).with("automation/filea.sl20.rb").ordered
       expect(subject).to receive(:load).with("automation/sl30/fileb.rb").ordered
