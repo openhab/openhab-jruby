c6cad4546169755ed51c898598526efd5ff61154
jimtng <2554958+jimtng@users.noreply.github.com>
2023-03-16 02:24:41 +1000

    strip links in html description (#34)
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- templates/default/layout/html/setup.rb
+++ templates/default/layout/html/setup.rb
@@ -6,7 +6,14 @@ end
 
 def meta_description
   default = "Documentation for openhab-scripting JRuby helper library."
-  return "#{object.docstring} - #{default}" unless object.docstring.blank?
+  return "#{strip_links(object.docstring)} - #{default}" unless object.docstring.blank?
 
   @file&.attributes&.dig(:description) || default
+end
+
+# Extracts the description from YARD-style and markdown-style links
+#   "{ClassName} text [Title](url)" => "ClassName text Title"
+#   "{ClassName Class Title} text [Title](url)" => "Class Title text Title"
+def strip_links(text)
+  text.gsub(/\{(.+?)\}/) { $1.split(" ", 2).last }.gsub(/\[(.*?)\]\(.*?\)/, "\\1")
 end
