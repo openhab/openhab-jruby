9c710c273480901bd92cf0b5779cdc5ce9bc55a5
Cody Cutrer <cody@cutrer.us>
2023-04-19 11:11:29 -0600

    update docs automatically after release
     * add rake task to update docs to mark latest minor release as stable
     * call it from the release action
     * have the release action call the YARD doc action, both for main and the new tag
    
    and update the missed minor release locally
    
    Signed-off-by: Cody Cutrer <cody@cutrer.us>

--- templates/default/fulldoc/html/js/app.js
+++ templates/default/fulldoc/html/js/app.js
@@ -211,7 +211,7 @@ function enableHovers() {
 }
 
 function selectVersion() {
-  if (document.location.pathname.startsWith("/openhab-jruby/5.0/")) {
+  if (document.location.pathname.startsWith("/openhab-jruby/5.1/")) {
     $(".version-button.stable").toggleClass("current");
   } else if (document.location.pathname.startsWith("/docs/") ||
     document.location.pathname.startsWith("/openhab-jruby/main/") ||
