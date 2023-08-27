f69f98025e71ce7decaf44b0a52109dd04268eae
Jimmy Tanagra <jcode@tanagra.id.au>
2023-08-27 11:56:30 +1000

    Add `event#inputs` to AbstractEvent
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- lib/openhab/dsl/rules/automation_rule.rb
+++ lib/openhab/dsl/rules/automation_rule.rb
@@ -143,6 +143,8 @@ module OpenHAB
             end
 
             event.attachment = attachment
+            # WatchEvent and future custom events may not need/support inputs
+            event.inputs = inputs if event.respond_to?(:inputs=)
             return event
           end
 
