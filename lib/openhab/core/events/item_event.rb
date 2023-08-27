b8dde7ab0dd1bbb7ffb6fa378678f5de0127e59a
Jimmy Tanagra <jcode@tanagra.id.au>
2023-08-27 11:57:02 +1000

    Add `event.group` attribute for triggeringGroup
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- lib/openhab/core/events/item_event.rb
+++ lib/openhab/core/events/item_event.rb
@@ -16,6 +16,23 @@ module OpenHAB
         def item
           EntityLookup.lookup_item(item_name)
         end
+
+        #
+        # @!attribute [r] group
+        #
+        # The group item whose member had triggered this event.
+        #
+        # This is the equivalent of openHAB's `triggeringGroup`, and it is only available
+        # on a member-of-group trigger.
+        #
+        # @return [Item,nil] The group item whose member had triggered this event.
+        #   `nil` when the event wasn't triggered by a member-of-group trigger.
+        #
+        # @since openHAB 4.0
+        #
+        def group
+          inputs["triggeringGroup"]&.then { |triggering_group| Items::Proxy.new(triggering_group) }
+        end
       end
     end
   end
