c42cabc9ff82e55cd010cbfbf5a0f3659c681078
Jimmy Tanagra <jcode@tanagra.id.au>
2023-09-08 11:10:25 +1000

    Revert "Add `event.inputs`; and `event.group` attributes for triggeringGroup (#123)"
    This reverts commit 8cb45f6fc826d0e251a992a63f05a0a4576caef5.
    
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- lib/openhab/core/events/abstract_event.rb
+++ lib/openhab/core/events/abstract_event.rb
@@ -10,9 +10,6 @@ module OpenHAB
         # @return [Object]
         attr_accessor :attachment
 
-        # @return [Hash]
-        attr_accessor :inputs
-
         # @return [String]
         alias_method :inspect, :to_s
 
