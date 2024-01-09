cef8d4c298e4ad823a9496cc7fc163ed16cecef2
Jimmy Tanagra <jcode@tanagra.id.au>
2024-01-10 09:55:28 +1000

    Add description and tags to terse rules
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- spec/openhab/dsl/rules/terse_spec.rb
+++ spec/openhab/dsl/rules/terse_spec.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe OpenHAB::DSL::Rules::Terse do
+  before do
+    items.build { switch_item "TestSwitch" }
+  end
+
   it "works" do
     this = self
-    items.build { switch_item "TestSwitch" }
     ran = false
     changed TestSwitch do
       ran = true
@@ -13,19 +16,48 @@ RSpec.describe OpenHAB::DSL::Rules::Terse do
     expect(ran).to be true
   end
 
+  it "returns the rule object" do
+    rule = changed(TestSwitch) { nil }
+    expect(rule).to be_a OpenHAB::Core::Rules::Rule
+  end
+
   it "requires a block" do
-    items.build { switch_item "TestSwitch" }
     expect { changed(TestSwitch) }.to raise_error(ArgumentError)
   end
 
   it "can also run on_load" do
     this = self
-    items.build { switch_item "TestSwitch" }
     ran = false
     changed TestSwitch, on_load: true do
       ran = true
       expect(self).to be this
     end
     expect(ran).to be true
+  end
+
+  it "can define the rule name" do
+    rule = changed(TestSwitch, name: "Test") { nil }
+    expect(rule.name).to eql "Test"
+  end
+
+  it "can define the rule description" do
+    rule = changed(TestSwitch, description: "Test") { nil }
+    expect(rule.description).to eql "Test"
+  end
+
+  it "can define the rule id" do
+    rule = changed(TestSwitch, id: "Test") { nil }
+    expect(rule.uid).to eql "Test"
+  end
+
+  it "can define the rule tags" do
+    rule = changed(TestSwitch, tag: "Test") { nil }
+    expect(rule.tags).to match_array ["Test"]
+
+    rule = changed(TestSwitch, tag: Semantics::LivingRoom) { nil }
+    expect(rule.tags).to match_array ["LivingRoom"]
+
+    rule = changed(TestSwitch, tags: %w[Test1 Test2]) { nil }
+    expect(rule.tags).to match_array %w[Test1 Test2]
   end
 end
