c8c884142df6cda225b4b3445bbe5e33b396c5b5
Jimmy Tanagra <jcode@tanagra.id.au>
2024-01-24 09:31:28 +1000

    Add NumericItem#nonzero?
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- lib/openhab/core/items/numeric_item.rb
+++ lib/openhab/core/items/numeric_item.rb
@@ -7,7 +7,7 @@ module OpenHAB
     module Items
       # Mixin for implementing type coercion for number-like items
       module NumericItem
-        %i[positive? negative? zero?].each do |predicate|
+        %i[positive? negative? zero? nonzero?].each do |predicate|
           class_eval <<~RUBY, __FILE__, __LINE__ + 1
             def #{predicate}              # def positive?
               return false unless state?  #   return false unless state?
