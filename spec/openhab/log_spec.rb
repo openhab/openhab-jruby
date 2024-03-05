fc7d9dfaf62c971f17cc3b9df1788cf54e0624ae
jimtng <2554958+jimtng@users.noreply.github.com>
2024-03-06 01:27:10 +1000

    Tweak Rule ID inference to work with F7 router (#260)
    Framework7 doesn't like dots in the dynamic route path.
    Accessing the script using the url /settings/scripts/filename.rb:xx will
    result in a 404 because no route would match.
    
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- spec/openhab/log_spec.rb
+++ spec/openhab/log_spec.rb
@@ -65,16 +65,18 @@ RSpec.describe OpenHAB::Log do
 
     it "uses the rule id inside a timer block inside a rule" do
       executed = false
+      logger_name = nil
       rule "log test" do
         on_load
         run do
           after(1.second) do
             executed = true
-            expect(logger.name).to match(/^org\.openhab\.automation\.jrubyscripting\.rule\.log_spec\.rb:(?:\d+)$/)
+            logger_name = logger.name
           end
         end
       end
       time_travel_and_execute_timers(5.seconds)
+      expect(logger_name).to match(/^org\.openhab\.automation\.jrubyscripting\.rule\.log_spec:(?:\d+)$/)
       expect(executed).to be true
     end
 
