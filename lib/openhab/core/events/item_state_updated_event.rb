d36eed8213175744f1881b08ce40bc916fa3b28b
Jimmy Tanagra <jcode@tanagra.id.au>
2025-02-17 16:54:49 +1000

    Support `ItemStateUpdatedEvent#last_state_update` and `ItemStateChangedEvent#last_state_change`
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- lib/openhab/core/events/item_state_updated_event.rb
+++ lib/openhab/core/events/item_state_updated_event.rb
@@ -14,6 +14,19 @@ module OpenHAB
       #
       class ItemStateUpdatedEvent < ItemEvent
         include ItemState
+
+        # @!attribute [r] last_state_update
+        #   @return [ZonedDateTime] the time the previous state update occurred
+        #   @since openHAB 5.0
+
+        # @return [String]
+        def inspect
+          s = "#<OpenHAB::Core::Events::ItemStateUpdatedEvent item=#{item_name} state=#{item_state.inspect}"
+          # @deprecated OH4.3 remove respond_to? check when dropping OH 4.3
+          s += " last_state_update=#{last_state_update}" if respond_to?(:last_state_update) && last_state_update
+          s += " source=#{source.inspect}" if source
+          "#{s}>"
+        end
       end
     end
   end
--- spec/openhab/core/events/item_state_updated_event_spec.rb
+++ spec/openhab/core/events/item_state_updated_event_spec.rb
@@ -5,14 +5,18 @@ RSpec.describe OpenHAB::Core::Events::ItemStateUpdatedEvent do
     event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", NULL, nil)
     expect(event.inspect).to eql "#<OpenHAB::Core::Events::ItemStateUpdatedEvent item=item state=NULL>"
 
-    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", NULL, "source")
+    # @deprecated OH4.3 remove args and pass 4 arguments directly when dropping oh 4.3
+    args = ["item", NULL]
+    args << nil if OpenHAB::Core.full_version > Gem::Version.new("5.0.0.M1")
+    args << "source"
+    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event(*args)
     expect(event.inspect).to eql(
       '#<OpenHAB::Core::Events::ItemStateUpdatedEvent item=item state=NULL source="source">'
     )
   end
 
   it "has proper predicates for a NULL event" do
-    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", NULL)
+    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", NULL, nil)
 
     expect(event).to be_null
     expect(event).not_to be_undef
@@ -21,7 +25,7 @@ RSpec.describe OpenHAB::Core::Events::ItemStateUpdatedEvent do
   end
 
   it "has proper predicates for an UNDEF event" do
-    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", UNDEF)
+    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", UNDEF, nil)
 
     expect(event).not_to be_null
     expect(event).to be_undef
@@ -30,7 +34,7 @@ RSpec.describe OpenHAB::Core::Events::ItemStateUpdatedEvent do
   end
 
   it "has proper predicates for an ON event" do
-    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", ON)
+    event = OpenHAB::Core::Events::ItemEventFactory.create_state_updated_event("item", ON, nil)
 
     expect(event).not_to be_null
     expect(event).not_to be_undef
