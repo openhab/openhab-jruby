4fe9785f36962f1a8b79d73694d5ece74f075437
Jimmy Tanagra <jcode@tanagra.id.au>
2025-03-12 00:20:43 +1000

    add JLine-based REPL and IRB scripts for Karaf console
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

---
+++ lib/openhab/console/jline.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require "openhab/console/stdio"
+
+module OpenHAB
+  module Console
+    #
+    # A basic JRuby REPL for openHAB Karaf console
+    #
+    # To use this, set the jrubyscripting add-on `console` configuration to `jline`
+    #
+    class JLine
+      # Create constants instead of java_import to avoid polluting the global namespace
+      LineReader = org.jline.reader.LineReader
+      Bracket = org.jline.reader.impl.DefaultParser::Bracket
+
+      BOLD = "\e[1m"
+      RESET = "\e[0m"
+      RED = "\e[31m"
+      GREEN = "\e[32m"
+      YELLOW = "\e[33m"
+      BLUE = "\e[34m"
+      CYAN = "\e[36m"
+
+      ERROR = RED
+      STRING = BOLD + YELLOW
+      NUMBER = BOLD + BLUE
+      OBJECT = BOLD + GREEN
+      SIMPLE_CLASS = BOLD + CYAN
+      PROMPT = "#{BOLD}JRuby> #{RESET}"
+
+      class << self
+        def start
+          puts "Welcome to JRuby REPL. Press Ctrl+D to exit, Alt+Enter (or Esc,Enter) to insert a new line."
+
+          console = new
+
+          loop do
+            begin
+              input = console.read_line
+              next if input.strip.empty?
+            rescue org.jline.reader.UserInterruptException # Ctrl+C is pressed
+              next
+            rescue org.jline.reader.EndOfFileException # Ctrl+D is pressed
+              break
+            end
+
+            begin
+              # Use TOPLEVEL_BINDING to isolate and keep the local variables between loops
+              result = TOPLEVEL_BINDING.eval(input)
+              console.print_result(result)
+            rescue Exception => e
+              console.print_error(e)
+            end
+          end
+        end
+      end
+
+      def initialize
+        parser = org.jline.reader.impl.DefaultParser.new
+                    .eof_on_unclosed_bracket(Bracket::CURLY, Bracket::ROUND, Bracket::SQUARE)
+                    .eof_on_unclosed_quote(true)
+                    .eof_on_escaped_new_line(true)
+
+        completer =
+          org.jline.reader.Completer.impl do |_method_name, _reader, _line, candidates|
+            sources = TOPLEVEL_BINDING.local_variables + Object.constants
+            sources += DSL.items.map(&:name) + DSL.methods(false) if defined?(DSL)
+
+            candidates.add_all(sources.map { |c| org.jline.reader.Candidate.new(c.to_s) })
+          end
+
+        @reader = org.jline.reader.LineReaderBuilder.builder
+                     .terminal($terminal)
+                     .app_name("jrubyscripting")
+                     .parser(parser)
+                     .completer(completer)
+                     .variable(LineReader::SECONDARY_PROMPT_PATTERN, "%M%P > ")
+                     .variable(LineReader::INDENTATION, 2)
+                     .build
+      end
+
+      def read_line
+        @reader.read_line(PROMPT)
+      end
+
+      def print_result(result)
+        puts "=> " + # rubocop:disable Style/StringConcatenation
+             case result
+             when nil, true, false then SIMPLE_CLASS + result.inspect + RESET
+             when String then %("#{STRING}#{result.dump[1..-2]}#{RESET}")
+             when Numeric, Array, Hash then NUMBER + result.to_s + RESET
+             else OBJECT + result.inspect + RESET
+             end
+      end
+
+      def print_error(error)
+        puts ERROR + "Error: #{error.message}" + RESET
+      end
+    end
+  end
+end
+
+OpenHAB::Console::JLine.start
---
+++ lib/openhab/console/stdio.rb
@@ -0,0 +1,182 @@
+# frozen_string_literal: true
+
+require "openhab/console"
+require "stringio"
+
+module OpenHAB
+  # @!visibility private
+  module Console
+    class Stdio
+      attr_reader :internal_encoding
+
+      def initialize(terminal)
+        @terminal = terminal
+        @external_encoding = Encoding.find(@terminal.encoding.to_s)
+      end
+
+      def set_encoding(_external, internal = nil, _options = {})
+        @internal_encoding = internal
+      end
+
+      def tty?
+        true
+      end
+
+      def winsize
+        [@terminal.width, @terminal.height]
+      end
+
+      def inspect
+        "#<#{self.class}>"
+      end
+    end
+
+    class Stdin < Stdio
+      attr_reader :external_encoding, :internal_encoding
+
+      def initialize(terminal)
+        super
+
+        @byte_stream = terminal.input
+        @byte_stream.class.field_reader :readBuffer
+        @wait_method = @byte_stream.java_class.get_declared_method("wait", java.nio.ByteBuffer, Java::long)
+        @wait_method.accessible = true
+        @buffer = StringIO.new.set_encoding(external_encoding)
+      end
+
+      def getbyte
+        unless @buffer.eof?
+          b = @buffer.getbyte
+          @buffer.truncate(0) if @buffer.eof?
+          return b
+        end
+
+        b = @byte_stream.read
+        return nil if b.negative?
+
+        b
+      end
+
+      def getc
+        unless @buffer.eof?
+          c = @buffer.getc
+          @buffer.truncate(0) if @buffer.eof?
+          return c
+        end
+        bytes = (+"").force_encoding(Encoding::BINARY)
+        loop do
+          b = getbyte
+          return nil if b.nil?
+
+          bytes << b.chr
+          c = bytes.encode(external_encoding,
+                           internal_encoding || Encoding.default_internal,
+                           invalid: :replace,
+                           undef: :replace,
+                           replace: "")
+          return c unless c.empty?
+        end
+      rescue java.io.InterruptedIOException
+        raise Interrupt
+      end
+
+      def ungetbyte(byte)
+        @buffer.ungetbyte(byte)
+      end
+
+      def ungetc(char)
+        @buffer.ungetc(char)
+      end
+
+      def gets
+        result = +""
+        loop do
+          c = getc
+          if c.nil?
+            return nil if result.empty?
+
+            break
+          end
+
+          if c == "\x04" && result.empty?
+            return nil
+          elsif c == "\x7f"
+            result.slice(0...-1)
+          else
+            result << c
+          end
+          break if c == "\n"
+        end
+        result
+      rescue java.io.InterruptedIOException
+        raise Interrupt
+      end
+
+      def read(bytes)
+        r = readpartial(bytes)
+        r.concat(readpartial(bytes - r.bytesize)) while r.bytesize < bytes
+        r
+      end
+
+      def readpartial(bytes)
+        available = @buffer.size - @buffer.tell
+        if available.positive?
+          bytes = available if available < bytes
+          r = @buffer.read(bytes)
+          @buffer.truncate(0) if @buffer.eof?
+          return r
+        end
+
+        buffer = Java::byte[bytes].new
+        read = @byte_stream.read_buffered(buffer)
+        buffer = buffer[0..read] if read != bytes
+        String.from_java_bytes(buffer)
+      end
+      alias_method :read_nonblock, :readpartial
+
+      def wait_readable(timeout = nil)
+        @byte_stream.synchronized do
+          return true if @byte_stream.readBuffer.has_remaining?
+
+          timeout = timeout ? 0 : timeout * 1000.to_i
+          r = @wait_method.invoke(@byte_stream, @byte_stream.readBuffer, timeout)
+          return nil if r.negative?
+
+          self
+        end
+      end
+
+      def raw(*)
+        previous_attributes = @terminal.enter_raw_mode
+        yield self
+      ensure
+        @terminal.set_attributes(previous_attributes)
+      end
+    end
+
+    class Stdout < Stdio
+      def initialize(terminal)
+        super
+        @writer = terminal.writer
+      end
+
+      def write(output)
+        @writer.print(output)
+        @writer.flush
+      end
+      alias_method :<<, :write
+
+      def flush
+        @writer.flush
+      end
+
+      def puts(output)
+        output = output.join("\n") if output.is_a?(Array)
+        @writer.println(output.to_s)
+      end
+    end
+  end
+end
+
+$stdin = OpenHAB::Console::Stdin.new($terminal)
+$stdout = $stderr = OpenHAB::Console::Stdout.new($terminal)
