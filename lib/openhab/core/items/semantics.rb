e1d34bd7bd1ff9d4bf2e2eafce42b74d5fa14135
jimtng <2554958+jimtng@users.noreply.github.com>
2025-03-16 07:35:26 +1000

    Use #ruby_first to solve Java21's List#first incompatibility (#383)
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- lib/openhab/core/items/semantics.rb
+++ lib/openhab/core/items/semantics.rb
@@ -209,9 +209,10 @@ module OpenHAB
           def lookup(id, locale = java.util.Locale.default)
             id = id.to_s
 
-            tag = service.get_by_label_or_synonym(id, locale)
-            tag = nil if tag&.empty?
-            tag_class = tag&.first ||
+            # Java21 added #first method, which overrides Ruby's #first.
+            # It throws an error if the list is Empty instead of returning nil.
+            # So we use #ruby_first to ensure we get Ruby's behaviour
+            tag_class = service.get_by_label_or_synonym(id, locale).ruby_first ||
                         Provider.registry.get_tag_class_by_id(id)
             return unless tag_class
 
